5592a258784c6a551258d44557635605e60674c1
Loveyless <88988179+Loveyless@users.noreply.github.com>
2022-05-01 15:38:36 +0800

    解决打包页面空白

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const Components = require("unplugin-vue-components/webpack");
 const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 
 module.exports = {
+  publicPath: "./",
   configureWebpack: {
     plugins: [
       AutoImport({
